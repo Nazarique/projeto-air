0f98818f6188cfdbb5d3966ba9b05e7dd067f7aa
Henrique Naźario <nazario.utfpr@gmail.com>
2020-06-02 15:07:38 -0300

    os pinouts foram modificados para a proposta atual

--- PINOUT.h
+++ PINOUT.h
@@ -8,31 +8,37 @@
   #endif
 
 	//		 		ENCODER_AS5045_SPI
-	#define 		AS_SPI_SCK 					52  // clk
-	#define 		AS_SPI_MISO 				50  // DO pin
-	#define 		AS_SPI_MOSI 				51  // não usado
-	#define 		AS_SPI_SS 					48  // CS for this device
+	#define 		AS_SPI_SCK 					52    // clk - 
+	#define 		AS_SPI_MISO 				50    // DO pin
+	#define 		AS_SPI_SS 					48    // CS for this device
+
 
 	//				MOTORDRIVER_BTS7960
-	#define			DRIVE_EN 					13
+	#define			DRIVE_EN_R 					53
+	#define			DRIVE_EN_L 					51
 	#define			DRIVE_PWM_L					12
 	#define			DRIVE_PWM_R  				11
 
 	//		 		DISPLAY_LCD_20x4
-	#define			LCD_DB4 					4	
-	#define			LCD_DB5 					5
-	#define			LCD_DB6 					6
-	#define			LCD_DB7 					7
+	#define			LCD_DB4 					49	
+	#define			LCD_DB5 					47
+	#define			LCD_DB6 					45
+	#define			LCD_DB7 					43
 	#define			LCD_RS 						8
 	#define			LCD_EN 						9
+	#define			LCD_PWM						13
+
 
 	// 				BOTÕES
-    #define         B_bt1                       32
-    #define         B_bt2                       34
-    #define         B_bt3                       36
-    #define         B_bt4                       38
-    #define         B_bt5                       40
-    #define         B_bt6                       30
+    #define         B_bt1                       41
+    #define         B_bt2                       39
+    #define         B_bt3                       37
+    #define         B_bt4                       35
+    #define         B_bt5                       33
+    #define         B_bt6                       31
+
+    #define         P_VALVULA_PRESSAO_EXP		46	
+    #define         P_SENSOR_PRESSAO			1	
 
   #ifdef __cplusplus
   } // extern "C"
