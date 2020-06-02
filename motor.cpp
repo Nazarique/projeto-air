9d607fa64b59eb26d70722a7702390950a129322
Henrique Naźario <nazario.utfpr@gmail.com>
2020-06-02 15:07:09 -0300

    Alterações para o drive BTS7960 usando dois enable e com controle via dois PWM

--- motor.cpp
+++ motor.cpp
@@ -1,35 +1,39 @@
 #include "bibliotecas.h"
 
 void stop_Motor()
-{           
-  digitalWrite(DRIVE_EN, LOW);                       
+{                            
+  digitalWrite(DRIVE_EN_L, LOW);
+  digitalWrite(DRIVE_EN_R, LOW);
   OCR1A = 0;
   OCR1B = 0;
 } 
 
 void direct_Motor(uint8_t p, uint8_t duty)                                                    
 {
-  digitalWrite(DRIVE_EN, HIGH);
+  
   if(p==D_ROTACAO_0_DESCIDA)                                                
   {
+  	digitalWrite(DRIVE_EN_L, LOW);
     OCR1A = duty;
-    OCR1B = 0;                                                 
+    OCR1B = 0;    
+    digitalWrite(DRIVE_EN_R, HIGH);                                             
   }
   else if(p==D_ROTACAO_1_SUBIDA)
-  {                                                                    
+  { 
+  	digitalWrite(DRIVE_EN_R, LOW);                                                                   
     OCR1A = 0;
     OCR1B = duty;
+    digitalWrite(DRIVE_EN_L, HIGH);
   }
 } 
 
 void motor_Init()
 {
-  TCCR1A = 0b10100001; 
+  TCCR1A = 0b10101001; 
   TCCR1B = 0b00001010; 
 
   pinMode(DRIVE_PWM_R, OUTPUT);
   pinMode(DRIVE_PWM_L, OUTPUT);
-  pinMode(DRIVE_EN, OUTPUT);
-
-  digitalWrite(DRIVE_EN, HIGH);  
+  pinMode(DRIVE_EN_R, OUTPUT);
+  pinMode(DRIVE_EN_L, OUTPUT);
 }                                                                            
