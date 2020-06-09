0d63b356019ba55ce0e5890e11a4b40a7620f203
Henrique Naźario <nazario.utfpr@gmail.com>
2020-06-09 18:01:19 -0300

    codigo main usado para realizar testes

--- projeto-air.ino
+++ projeto-air.ino
@@ -7,25 +7,32 @@ void desliga()
 }
 void setup() 
 {
-  //button_Init();
+  button_Init();
   motor_Init();
-  //screen_Init();
+  screen_Init();
   interrupt4_OVF_Init();
   control_init();
 
-  set_sys_status(1);
+  set_sys_status(1);//inicializa controle
   Serial.begin(115200);
-
-  pinMode(2, INPUT);
-  attachInterrupt(digitalPinToInterrupt(2), desliga, FALLING);
+  pinMode(P_VALVULA_PRESSAO_EXP, INPUT);
+  attachInterrupt(digitalPinToInterrupt(P_VALVULA_PRESSAO_EXP), desliga, FALLING);
 }
 
 void loop() 
 {
-  //machine_state();
   system_status * p_sys_status;
   p_sys_status = get_sys_status();
 
-  Serial.print("Tempo: ");
-  Serial.println(p_sys_status->s_control.c_tempo_insp); 
+  Serial.print(" encoder: ");
+  Serial.print(p_sys_status->s_control.c_encoder);
+  Serial.print("\t tempo insp: ");
+  Serial.print(p_sys_status->s_control.c_tempo_insp);
+  Serial.print("\t tempo exp: ");
+  Serial.print(p_sys_status->s_control.c_tempo_exp);
+  Serial.print("\t pwm: ");
+  Serial.println(p_sys_status->s_control.c_pwm_atual);
+
+  //Serial.println( analogRead(P_SENSOR_PRESSAO));
+ 
 }
