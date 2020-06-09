3e6c811cda931dabc0ef6a695196ff678a89b788
Henrique Naźario <nazario.utfpr@gmail.com>
2020-06-09 18:00:26 -0300

    O dead time foi ajustado para 350ms e o degrau com delta de tempo de 5ms e um delta de pwm de 10 pontos.
    A leitura do encoder esta sendo realizada sem o casting, usando uma resolução de 2^12, uma contagem de tempo
    de inspiração e de expiração foi adicionado, além deuma variavel que recebe o valor atual do encoder, usada para testes.
    
    O Jakson adicionou mais uma condição para troca de estados
    
    O controle inicia com a expiração ou seja o motor inicia realizando subida

--- controle.cpp
+++ controle.cpp
@@ -33,15 +33,15 @@ void set_Degrau(control_t *motor)
       o motor na posição definida. */
   /* Contadores usados para controle do motor, cont200 = 200ms, cont5 = 5ms.*/
 
-  static uint8_t cont200 = 200;
+  static uint16_t cont35 = 350;
   static uint8_t cont5 = 5;
   static uint8_t degrau_Motor = 0;
 
   if(motor->c_deadTime_Motor)                                                        
   {
-    if(--cont200==0)                                                           
+    if(--cont35==0)                                                           
     { 
-      cont200 = 200;                                                            
+      cont35 = 350;                                                            
       motor->c_deadTime_Motor = 0; 
     }
   }
@@ -58,7 +58,7 @@ void set_Degrau(control_t *motor)
   else if(!degrau_Motor)
   {
     motor->c_pwm_atual = degrau(motor->c_pwm_requerido, 
-                              motor->c_pwm_atual);
+                                motor->c_pwm_atual);
     degrau_Motor = 1;
     direct_Motor(motor->c_direcao, motor->c_pwm_atual);
   }
@@ -86,19 +86,24 @@ void control_Inspiracao(system_status *p_sys_status)
       de inspiração.
   */
   uint16_t posicao_encoder;
-
-  posicao_encoder = ((uint16_t)(encoder.read()*0.08789));
-
-  digitalWrite(P_VALVULA_PRESSAO_EXP, HIGH);
-  p_sys_status->s_control.c_tempo_insp++;
+  static uint32_t cont_time = 0;
   
-  if (posicao_encoder > (p_sys_status->s_control.c_angulo_final))
+  posicao_encoder = encoder.read();
+  
+  p_sys_status->s_control.c_encoder = posicao_encoder;
+  
+  cont_time++;
+  
+  digitalWrite(P_VALVULA_PRESSAO_EXP, HIGH);
+  
+  if (posicao_encoder < (p_sys_status->s_control.c_angulo_final) &  posicao_encoder > 200)
   { 
     p_sys_status->s_control.c_direcao = 0;
     p_sys_status->s_control.c_pwm_requerido = 250;
     p_sys_status->s_control.c_pwm_atual = 0; 
     p_sys_status->s_control.c_deadTime_Motor = 1;
-    p_sys_status->s_control.c_tempo_insp = 0;
+    p_sys_status->s_control.c_tempo_insp = cont_time;
+    cont_time = 0;
     stop_Motor();        
     PonteiroDeFuncao = control_Expiracao;
   }
@@ -111,10 +116,14 @@ void control_Expiracao(system_status *p_sys_status)
       função para execução dos procedimentos durante a fase
       de expiração.
   */
+  static uint32_t cont_time = 0;
   
   uint16_t posicao_encoder;
-  posicao_encoder = ((uint16_t)(encoder.read()*0.08789));
+  posicao_encoder = encoder.read();
   
+  p_sys_status->s_control.c_encoder = posicao_encoder;
+  
+  cont_time++;
   if(analogRead(P_SENSOR_PRESSAO) < p_sys_status->s_control.c_pressao_PEEP)
   {
     digitalWrite(P_VALVULA_PRESSAO_EXP, HIGH);
@@ -123,13 +132,15 @@ void control_Expiracao(system_status *p_sys_status)
   {
     digitalWrite(P_VALVULA_PRESSAO_EXP, LOW); 
   }
-
-  if(posicao_encoder < (p_sys_status->s_control.c_angulo_inicial))
+  
+  if(posicao_encoder > (p_sys_status->s_control.c_angulo_inicial)|posicao_encoder < 200)
   { 
     p_sys_status->s_control.c_direcao = 1;
     p_sys_status->s_control.c_pwm_requerido = p_sys_status->s_control.c_pwm_insp;
     p_sys_status->s_control.c_pwm_atual= 0;
     p_sys_status->s_control.c_deadTime_Motor = 1;
+    p_sys_status->s_control.c_tempo_exp = cont_time;
+    cont_time = 0;
     stop_Motor();   
     PonteiroDeFuncao = control_Inspiracao;
   }
@@ -147,11 +158,11 @@ void control_init()
   pinMode(P_VALVULA_PRESSAO_EXP, OUTPUT);
   encoder.begin();
   
-  p_sys_status->s_control.c_angulo_inicial = 60;
-  p_sys_status->s_control.c_angulo_final = 120;
+  p_sys_status->s_control.c_angulo_inicial = 4000;
+  p_sys_status->s_control.c_angulo_final = 3100;
   p_sys_status->s_control.c_pressao_PEEP = 6;
-  p_sys_status->s_control.c_pwm_insp = 120;
+  p_sys_status->s_control.c_pwm_insp = 150;
   p_sys_status->s_control.c_pwm_requerido = p_sys_status->s_control.c_pwm_insp;
 
-  PonteiroDeFuncao = control_Inspiracao;
+  PonteiroDeFuncao = control_Expiracao;
 }
