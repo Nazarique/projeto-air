313fe6fda53c80ebe97a55e29d6ffc615bb27b97
Henrique Naźario <nazario.utfpr@gmail.com>
2020-06-09 17:55:24 -0300

    Corrigido o tempo de estouro de interrupção, estava configurado para 8ms OVF, foi alterado para 1ms de OVF

--- timers.cpp
+++ timers.cpp
@@ -10,7 +10,7 @@ ISR(TIMER4_OVF_vect)
 void interrupt4_OVF_Init()
 {  
   TCCR4A = 0x00;                                                               
-  TCCR4B = 0x02;                                                               
+  TCCR4B = 0x01;                                                               
   TCNT4 = 49536;                                                               
   TIMSK4 = 0x01;          
   // --- Time counter =  ( ( 2^16 - 49536)   x   (Prescaler=1)  /  16E+6 = 1ms ---                                                     
