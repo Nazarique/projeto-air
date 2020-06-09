44b32d156f0f99d0dba0e30e72d952fc60383256
Henrique Naźario <nazario.utfpr@gmail.com>
2020-06-09 08:33:17 -0300

    codigo main para testes do dia 09/06

--- projeto-air.ino
+++ projeto-air.ino
@@ -1,23 +1,31 @@
 #include "bibliotecas.h"
 
-void setup() {
-  button_Init();
+void desliga()
+{
+  set_sys_status(0);
+  stop_Motor();
+}
+void setup() 
+{
+  //button_Init();
   motor_Init();
   //screen_Init();
   interrupt4_OVF_Init();
-  Serial.begin(115200);
   control_init();
+
   set_sys_status(1);
-  system_status * p_sys_status;
-  p_sys_status = get_sys_status();
-  
+  Serial.begin(115200);
+
+  pinMode(2, INPUT);
+  attachInterrupt(digitalPinToInterrupt(2), desliga, FALLING);
 }
 
-void loop() {
+void loop() 
+{
   //machine_state();
   system_status * p_sys_status;
   p_sys_status = get_sys_status();
+
   Serial.print("Tempo: ");
-  Serial.println(p_sys_status->s_control.c_tempo_insp);
- 
+  Serial.println(p_sys_status->s_control.c_tempo_insp); 
 }
