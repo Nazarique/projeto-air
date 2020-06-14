0af01a974a0f0a3bfb97255c30f9ca80c44070fd
Henrique Naźario <nazario.utfpr@gmail.com>
2020-06-14 12:50:28 -0300

    trocando os char para formar U T F P R !

--- botao.h
+++ botao.h
@@ -8,23 +8,23 @@
 	#endif
 
 
-		#define			D_CARACTER_BOTAO_1	80
-		#define			D_CARACTER_BOTAO_2	81
-		#define			D_CARACTER_BOTAO_3	82
-		#define			D_CARACTER_BOTAO_4	83
-		#define			D_CARACTER_BOTAO_5	84
-		#define			D_CARACTER_BOTAO_6	85
+		#define			D_CARACTER_BOTAO_1	85
+		#define			D_CARACTER_BOTAO_2	85
+		#define			D_CARACTER_BOTAO_3	70
+		#define			D_CARACTER_BOTAO_4	80
+		#define			D_CARACTER_BOTAO_5	82
+		#define			D_CARACTER_BOTAO_6	33
 		
 
 		typedef struct {
-		      unsigned char b1:1;
-		      unsigned char b2:1;
-		      unsigned char b3:1;
-		      unsigned char b4:1;
-		      unsigned char b5:1;
-		      unsigned char b6:1;
-		      unsigned char deadTime:1;
-		      unsigned char	:1;
+		    char b1:1;
+		    char b2:1;
+		    char b3:1;
+		    char b4:1;
+		    char b5:1;
+		    char b6:1;
+		    char deadTime:1;
+		    char   :1;
 		  } button_t;
 
 		char detect_Button();
