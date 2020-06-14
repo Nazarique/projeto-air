c144d779e9a424aac8a0389da0096d77fc5b8c56
Henrique Naźario <nazario.utfpr@gmail.com>
2020-06-14 17:37:03 -0300

    Coloquei definições de set para a tela dinamic, assim podemos criar "case" de acordo com a quantidade de var que vamos salvar, inc, dec ou apenas mostrar. As definições L_ se referem a limites das variaveis, exemplo: pause exp 350ms a 500ms. Alterei a struct config_t para ficar mais intuitivo na hora de programar, assim as variaveis aux que a IHM vai alterar vai ter o mesmo nome do que é printado na tela, mesmo que no controle a gente converta "volume" para "angulo".

--- tela.h
+++ tela.h
@@ -7,34 +7,69 @@
 	extern "C"{
 	#endif
 
+//						telas
 		#define 		D_TELA_CONFIG_0			'0'
 		#define 		D_TELA_CONFIG_1			'1'
 		#define 		D_MENU_PRESSAO			'2'
 		#define 		D_MENU_VOLUME		    '3'
-		#define 		D_MENU_FREQ_RES			'4'
-		#define 		D_MENU_TEMP_INS			'5'
+		#define			D_MENU_PEEP      		'4'
+		#define			D_MENU_CALIBRA      	'5'
 		#define 		D_TELA_INICIAL			'6'
-		#define			D_TELA_INICIAL_V 		'7'
 		#define			D_TELA_COLLAB      		'9'
 
+//						variáveis de controle
+		#define 		D_SET_VOLUME			'1'
+		#define 		D_SET_PEEP				'2'
+		#define 		D_SET_PRESSAO			'3'
+		#define 		D_SET_FREQ_RESPI		'4'
+		#define 		D_SET_TEMP_INSPI		'5'
+		#define 		D_SET_PAUSE_EXP			'6'
+		#define			D_SET_CALIBRA     		'7'
 
-	    #define 		D_INCREMENTO 			1 
-	    #define 		D_DECREMENTO 			2
+//						SET
+	    #define 		D_INCREMENTO 			 1 
+	    #define 		D_DECREMENTO 			 2
 	    
 
+// 		LIMITES das variáveis da IHM INF e SUP 
+		#define 		L_VOLUME_SUP			400
+		#define 		L_VOLUME_INF			100
+
+		#define 		L_PEEP_SUP				20
+		#define 		L_PEEP_INF				4
+
+		#define 		L_PRESSAO_SUP			60
+		#define 		L_PRESSAO_INF			10
+
+		#define 		L_FREQ_RESP_SUP			20
+		#define 		L_FREQ_RESP_INF			10
+
+		#define 		L_TEMP_INSP_SUP			60
+		#define 		L_TEMP_INSP_INF			10
+
+		#define 		L_PAUSE_EXP_SUP			500
+		#define 		L_PAUSE_EXP_INF			350
+
 		typedef struct {  
-	        uint8_t  pwm;
-	        uint8_t  PEEP;
-	        uint16_t angulo;
+	        uint16_t h_volume 	= 0;
+	        uint16_t h_peep   	= 0;
+	        uint16_t h_pressao	= 0;
+	        uint16_t h_freq		= 0;
+	        uint16_t h_temp_insp= 0;
+	        uint16_t h_pause_exp= 0;
 	    } config_t;
 
 		void machine_state();
 		void screen_static(char p);						//tela Static
 		void screen_dynamic(control_t *controle, config_t *IHM_aux, char p);	
 		void screen_Init();
-		void set_IHM_PWM(config_t *IHM_aux, uint8_t p);
-    void set_IHM_angulo(config_t *IHM_aux, uint8_t p);
-    void set_IHM_PEEP(config_t *IHM_aux, uint8_t p);
+
+		void set_IHM_volume(config_t *IHM_aux, uint8_t p);
+		void set_IHM_peep(config_t *IHM_aux, uint8_t p);
+		void set_IHM_pressao(config_t *IHM_aux, uint8_t p);
+		void set_IHM_freqResp(config_t *IHM_aux, uint8_t p);
+		void set_IHM_tempInsp(config_t *IHM_aux, uint8_t p);
+		void set_IHM_pauseExp(config_t *IHM_aux, uint8_t p);
 
 	#ifdef __cplusplus
 	} // extern "C"
