b05786a24f684f428826a58b8001031529ad1559
Pedro Dell <pedrofelipe@soulindo.com>
2020-06-24 16:22:27 -0300

    tela de pressao comecada

--- controle.h
+++ controle.h
@@ -6,16 +6,19 @@
   #ifdef __cplusplus
   extern "C"{
   #endif
-    
+    define ALARM_PRESSAO 
+    define ALARM_DESLIGADO 
+    define ALARM_PRESSAO 
+    define ALARM_PRESSAO 
+    define ALARM_PRESSAO 
+    define ALARM_PRESSAO 
     
     typedef void (*tipoFuncao_p)(system_status_t *p_sys_status);
 
     uint8_t rampa(uint8_t pwm, uint8_t pwm_atual);
     void set_rampa();
     void maqEstados_Control();
-    void control_Inspiracao_volume(system_status_t *p_sys_status);
-    void control_Inspiracao_pressao(system_status_t *p_sys_status);
-    
+    void control_Inspiracao(system_status_t *p_sys_status);
     void control_Expiracao(system_status_t *p_sys_status);
     
     uint8_t compensador(uint16_t tempo_inspiratorio_IHM,
