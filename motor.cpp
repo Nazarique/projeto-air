ed2e16d45bfaf23c6127a3b6e2785324eafae4b4
Henrique Nazário <nazario.utfpr@gmail.com>
2020-09-22 18:07:55 -0300

    foi trocado as defini~ções de subida e de descida para  o motor

--- motor.cpp
+++ motor.cpp
@@ -7,18 +7,18 @@ void stop_Motor()
   OCR1A = 0;
   OCR1B = 0;
 } 
-
+//----------------------------------------------------------------------------------------------------------------
 void direct_Motor(uint8_t p, uint8_t duty)                                                    
 {
   
-  if(p==D_ROTACAO_0_DESCIDA)                                                
+  if(p==D_ROTACAO_0_SUBIDA)                                                
   {
     OCR1B = duty;
     OCR1A = 0;
     digitalWrite(DRIVE_EN_R, HIGH);                                             
     digitalWrite(DRIVE_EN_L, HIGH);
   }
-  else if(p==D_ROTACAO_1_SUBIDA)
+  else if(p==D_ROTACAO_1_DESCIDA)
   { 
     OCR1A = duty;
     OCR1B = 0;
@@ -26,7 +26,7 @@ void direct_Motor(uint8_t p, uint8_t duty)
     digitalWrite(DRIVE_EN_L, HIGH);
   }
 } 
-
+//----------------------------------------------------------------------------------------------------------------
 void motor_Init()
 {
   TCCR1A = 0b10100001; 
