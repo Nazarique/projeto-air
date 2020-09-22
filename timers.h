7c61d536ed5358d73a856f39658a2c358f7474e0
Henrique Nazário <nazario.utfpr@gmail.com>
2020-09-22 18:12:07 -0300

    foi adcionado as inicializações dos pinos para buzzer e leds, além disso, foi usada a função memset para atribuir zero ao instanciar a struct de data

--- timers.h
+++ timers.h
@@ -3,22 +3,23 @@
 
 #include "bibliotecas.h"
 
-	#ifdef __cplusplus
-	extern "C"{
-	#endif
+#ifdef __cplusplus
 
-	typedef struct Data{
+extern "C"{
+#endif
 
-      float pressao;
-      uint16_t angulo;
-      uint8_t alarm;
-	} data;
- 
-		void interrupt4_OVF_Init();
-		void serial();
+typedef struct Data{
 
-	#ifdef __cplusplus
-	} // extern "C"
-	#endif
+	float pressao;
+	uint16_t angulo;
+	uint8_t alarm;
+} data;
+
+void interrupt4_OVF_Init();
+void serial();
+
+#ifdef __cplusplus
+} // extern "C"
+#endif
 
 #endif
